9cf1bab0fe06bfe6b1ea0c145852930a487d26dd
wangjun <wangjun@whdrawing.com>
2021-12-14 11:20:11 +0800

    s

--- tx/client_test.go
+++ tx/client_test.go
@@ -2,7 +2,6 @@ package tx
 
 import (
 	"fmt"
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"testing"
@@ -10,7 +9,7 @@ import (
 
 func TestClientBatchRequest(t *testing.T) {
 	client := NewWeb3Client("https://bsc-dataseed1.defibit.io/")
-	fq := ethereum.FilterQuery{
+	fq := FilterQuery{
 		Addresses: []common.Address{common.HexToAddress("0x7b4452dd6c38597fa9364ac8905c27ea44425832")},
 	}
 
