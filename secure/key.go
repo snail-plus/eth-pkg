c6af890d768b75c200204bb6dad35ce521090229
wangjun <wangjun@whdrawing.com>
2021-12-20 09:43:21 +0800

    s

--- secure/key.go
+++ secure/key.go
@@ -2,6 +2,7 @@ package secure
 
 import (
 	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -16,4 +17,23 @@ func StringToPrivateKey(privateKeyStr string) (*ecdsa.PrivateKey, error) {
 		return nil, err
 	}
 	return privateKey, nil
+}
+
+func PrivateKeyToAddressStr(privateKeyStr string) (string, error) {
+	address, err := PrivateKeyToAddress(privateKeyStr)
+	if err != nil {
+		return "", err
+	}
+
+	return address.String(), nil
+}
+
+func PrivateKeyToAddress(privateKeyStr string) (common.Address, error) {
+	key, err := StringToPrivateKey(privateKeyStr)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	address := crypto.PubkeyToAddress(key.PublicKey)
+	return address, nil
 }
