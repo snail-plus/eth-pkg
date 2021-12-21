feef1e9a3548ae52d42101f8f6d9477964eb192b
wangjun <wangjun@whdrawing.com>
2021-12-21 11:11:22 +0800

    s

--- tx/manager.go
+++ tx/manager.go
@@ -3,14 +3,16 @@ package tx
 import (
 	"context"
 	"github.com/snail-plus/eth-pkg/secure"
+	"log"
 	"math/big"
+	"strings"
 	"sync"
 )
 
 type TransactionManager interface {
 	// 返回hash
 	ExecuteTransaction(to string, data []byte, value *big.Int, gasPrice *big.Int, gasLimit uint64) (string, error)
-	GetNonce(ctx context.Context, account string) uint64
+	GetNonce(ctx context.Context, account string, refresh bool) (uint64, error)
 }
 
 type FastRawTransactionManager struct {
@@ -38,7 +40,10 @@ func (f *FastRawTransactionManager) ExecuteTransaction(to string, data []byte, v
 		return "", err
 	}
 
-	nonce := f.GetNonce(ctx, addressStr)
+	nonce, err := f.GetNonce(ctx, addressStr, false)
+	if err != nil {
+		return "", err
+	}
 
 	txInfo := TransactionInfo{
 		To:            to,
@@ -53,24 +58,29 @@ func (f *FastRawTransactionManager) ExecuteTransaction(to string, data []byte, v
 	}
 
 	txHash, err := f.web3Client.SendTransaction(ctx, signTx)
+	if err != nil && strings.Contains(err.Error(), "nonce too low") {
+		// nonce too low refresh
+		f.GetNonce(ctx, addressStr, true)
+	}
+
 	return txHash, err
 
 }
 
-func (f *FastRawTransactionManager) GetNonce(ctx context.Context, account string) uint64 {
+func (f *FastRawTransactionManager) GetNonce(ctx context.Context, account string, refresh bool) (uint64, error) {
 	f.mutex.Lock()
 	defer f.mutex.Unlock()
 
-	if f.nonce == 0 {
+	if f.nonce == 0 || refresh {
 		nonce, err := f.web3Client.GetNonce(ctx, account)
 		if err != nil {
-			return 1
+			return 0, err
 		}
-
+		log.Printf("address: %s, nonce: %d", account, nonce)
 		f.nonce = nonce
 	} else {
 		f.nonce = f.nonce + 1
 	}
 
-	return f.nonce
+	return f.nonce, nil
 }
